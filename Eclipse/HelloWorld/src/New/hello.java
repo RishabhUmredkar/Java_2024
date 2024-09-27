7f9b94abe7993df39a170a0cc4f58cf7198cb3d8
Rishabh Umredkar <umredkarrishabh@gmail.com>
2024-09-28 01:21:31 +0530

    java program

---
+++ Eclipse/HelloWorld/src/New/hello.java
@@ -0,0 +1,10 @@
+package New;
+
+import java.io.PrintStream;
+
+public class hello {
+public static void main(String[] args) {
+System.err.println("hello");	
+System.out.println("hiiiiiiiiii");
+}
+}
