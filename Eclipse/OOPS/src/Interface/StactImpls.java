3f09dc3736d04ae5fb7d365396d412694bf27d7a
Rishabh Umredkar <umredkarrishabh@gmail.com>
2024-10-13 23:44:46 +0530

    Interface program

---
+++ Eclipse/OOPS/src/Interface/StactImpls.java
@@ -0,0 +1,14 @@
+package Interface;
+
+public class StactImpls implements Stact{
+	@Override
+	public void add() {
+		// TODO Auto-generated method stub
+		System.out.println("this is add");
+	}
+	@Override
+	public void sub() {
+		// TODO Auto-generated method stub
+		System.out.println("thiss is sub");
+	}
+}
