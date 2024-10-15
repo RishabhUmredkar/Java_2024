ac9200fef0b7ceaa99b4bf2c352a842e42bc1453
Rishabh Umredkar <umredkarrishabh@gmail.com>
2024-10-15 23:15:21 +0530

    ToString program

---
+++ Eclipse/OOPS/src/Object/StudentDriver.java
@@ -0,0 +1,12 @@
+package Object;
+
+public class StudentDriver {
+
+	public static void main(String[] args) {
+		Student st1 = new Student("Rishabh", 21, "KDK",99.9,2024);
+		System.out.println(st1.toString());
+		System.out.println(st1.hashCode());
+		
+	}
+
+}
