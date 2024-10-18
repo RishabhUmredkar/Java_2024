9123d00b0602b019651cbd48e37ad0218f5fa27b
Rishabh Umredkar <umredkarrishabh@gmail.com>
2024-10-18 13:15:24 +0530

    Instagram App Update 1.0

---
+++ Eclipse/OOPS/src/Instagram_App/InstagramImpl2.java
@@ -0,0 +1,16 @@
+package Instagram_App;
+
+public class InstagramImpl2 extends InstagramImpl{
+
+	public InstagramImpl2(String name, String email, int pass) {
+		super(name, email, pass);
+	}
+	
+	@Override
+	public void message(String name, String msg) {
+		System.out.println(msg);
+		System.out.println(name + " Messaged");
+		System.out.println("Message Sent ...");
+		
+	}
+}
