9fb4b06b62c77807b238386fbe37f3553ef62292
Rishabh Umredkar <umredkarrishabh@gmail.com>
2024-10-23 00:51:49 +0530

    Checked Exception

---
+++ Eclipse/OOPS/src/CheckedException/Throws.java
@@ -0,0 +1,12 @@
+package CheckedException;
+
+public class Throws {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.out.println("Start");
+		Thread.sleep(10000);
+		System.out.println("End");
+	}
+
+}
